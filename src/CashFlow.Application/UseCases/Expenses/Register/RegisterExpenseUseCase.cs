b26a7bef7c71a136188b94fe4176d98c421f045d
aless <aless@ALESSANDRO>
2025-05-20 16:35:33 -0300

    Adicionar arquivos de projeto.

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,40 @@
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+public class RegisterExpenseUseCase
+{
+    public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+        Validate(request);
+
+        return new ResponseRegisteredExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+        if (titleIsEmpty)
+        {
+            throw new ArgumentException("The title is required");
+        }
+
+        if (request.Amount <= 0)
+        {
+            throw new ArgumentException("The Amount must be greater than zero");
+        }
+
+        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+        if (result > 0)
+        {
+            throw new ArgumentException("Expenses can't be for the future");
+        }
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+        if (!paymentTypeIsValid)
+        {
+            throw new ArgumentException("The paymentType is invalid. Try again and use another option");
+        }
+    }
+}
